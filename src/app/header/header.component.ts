b8f86e0d8699e013b6994fc8cf4e5c2a454fc0aa
Gilbert, Joe (CORP) <joe.gilbert@adp.com>
2020-03-27 17:42:26 -0400

    Databinding complete

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() menuLinkClicked = new EventEmitter<string>()
 
   collapsed = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onMenu(selection) {
+      this.menuLinkClicked.emit(selection);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,15 +7,20 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() itemClickEvent = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('test', 'This is simply a test.', 'https://images.media-allrecipes.com/userphotos/560x315/4565477.jpg'),
-    new Recipe('test', 'This is simply a test.', 'https://images.media-allrecipes.com/userphotos/560x315/4565477.jpg')
+    new Recipe('test', 'This is simply a test 1.', 'https://images.media-allrecipes.com/userphotos/560x315/4565477.jpg'),
+    new Recipe('test', 'This is simply a test 2.', 'https://images.media-allrecipes.com/userphotos/560x315/4565477.jpg')
 
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onItemClicked(recipe: Recipe) {
+    this.itemClickEvent.emit(recipe);
   }
 
 }
